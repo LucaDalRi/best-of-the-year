c2b32be158fe8783c95b789d39a41500a1036487
LucaDalRi <euro2xd@gmail.com>
2023-06-20 15:26:52 +0200

    Base progetto

---
+++ src/main/java/com/lessons/java/bestoftheyear/Controller/HomeController.java
@@ -0,0 +1,9 @@
+package com.lessons.java.bestoftheyear.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class HomeController {
+}
