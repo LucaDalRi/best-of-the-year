c990ee9fb37f36be06c1a3dbc70cc923a90f8bd5
LucaDalRi <euro2xd@gmail.com>
2023-06-20 16:02:43 +0200

    milestone 1 completata

--- src/main/java/com/lessons/java/bestoftheyear/Controller/HomeController.java
+++ src/main/java/com/lessons/java/bestoftheyear/Controller/HomeController.java
@@ -1,9 +1,18 @@
 package com.lessons.java.bestoftheyear.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/")
 public class HomeController {
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("name", "Luca Dal Rì");
+        return "index";
+    }
+
 }
